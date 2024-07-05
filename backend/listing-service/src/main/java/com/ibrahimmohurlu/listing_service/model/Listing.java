a6f3a01f4afe54937b3d6a31d1a51f78381a650e
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-05 16:38:17 +0300

    listing service models added.

---
+++ backend/listing-service/src/main/java/com/ibrahimmohurlu/listing_service/model/Listing.java
@@ -0,0 +1,52 @@
+package com.ibrahimmohurlu.listing_service.model;
+import com.ibrahimmohurlu.user_service.model.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "listings")
+@Data
+public class Listing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ListingStatus status;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.status = ListingStatus.IN_REVIEW;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
+
---
+++ backend/listing-service/src/main/java/com/ibrahimmohurlu/listing_service/model/ListingStatus.java
@@ -0,0 +1,7 @@
+package com.ibrahimmohurlu.listing_service.model;
+
+public enum ListingStatus {
+    ACTIVE,
+    INACTIVE,
+    IN_REVIEW
+}
