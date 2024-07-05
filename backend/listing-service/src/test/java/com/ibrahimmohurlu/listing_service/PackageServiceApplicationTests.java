bc7ac23eaf0448ab50c89dd27b1ad6ffa7b0c02f
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-05 16:37:36 +0300

    listing service init commit

---
+++ backend/listing-service/src/test/java/com/ibrahimmohurlu/listing_service/PackageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimmohurlu.listing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
