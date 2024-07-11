e25a35f3620a5077d709bc260fe91d7c036d3630
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-12 01:16:10 +0300

    user service eureka client added.

---
+++ backend/user-service/src/main/java/com/ibrahimmohurlu/user_service/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.ibrahimmohurlu.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @GetMapping
+    public String get(){
+        return "hello from user service";
+    }
+}
