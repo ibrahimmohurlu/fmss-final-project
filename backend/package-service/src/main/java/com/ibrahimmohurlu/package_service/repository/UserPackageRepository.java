bda7578d3492454596951b430febdab990b7cc47
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-15 01:17:06 +0300

    package service find active package query method removed.

--- backend/package-service/src/main/java/com/ibrahimmohurlu/package_service/repository/UserPackageRepository.java
+++ backend/package-service/src/main/java/com/ibrahimmohurlu/package_service/repository/UserPackageRepository.java
@@ -9,6 +9,5 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface UserPackageRepository extends JpaRepository<UserPackage, Long> {
-    @Query("SELECT up FROM UserPackage up WHERE up.user.id = :userId AND up.expirationDate > :currentDate AND up.remainingListingAllowance > 0")
-    List<UserPackage> findActiveUserPackage(@Param("userId") Long userId, @Param("currentDate") LocalDateTime currentDate);
+
 }
