7eced2533683ce620cd5c85a9af985c85ddcd52d
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-15 01:17:47 +0300

    package service unused import removed.

--- backend/package-service/src/main/java/com/ibrahimmohurlu/package_service/repository/UserPackageRepository.java
+++ backend/package-service/src/main/java/com/ibrahimmohurlu/package_service/repository/UserPackageRepository.java
@@ -2,11 +2,6 @@ package com.ibrahimmohurlu.package_service.repository;
 
 import com.ibrahimmohurlu.package_service.model.UserPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface UserPackageRepository extends JpaRepository<UserPackage, Long> {
 
