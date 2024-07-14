04feff7066b7456c5a13c8ec8cbcd0a039a3bd3b
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-14 17:16:22 +0300

    user service get user by email endpoint implemented.

--- backend/user-service/src/main/java/com/ibrahimmohurlu/user_service/controller/UserController.java
+++ backend/user-service/src/main/java/com/ibrahimmohurlu/user_service/controller/UserController.java
@@ -1,14 +1,34 @@
 package com.ibrahimmohurlu.user_service.controller;
 
+import com.ibrahimmohurlu.user_service.model.User;
+import com.ibrahimmohurlu.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
     @GetMapping
-    public String get(){
+    public String get() {
         return "hello from user service";
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        Optional<User> optionalUser = userService.getUserByEmail(email);
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optionalUser.get());
     }
 }
