e89c40c4432c839a5b8897303daa3974e273d792
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-20 16:47:33 +0300

    listing service added to the project.

---
+++ backend/listing-review-service/src/test/java/com/ibrahimmohurlu/listing_review_service/ListingReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimmohurlu.listing_review_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
