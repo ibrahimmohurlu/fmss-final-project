e7eea097c1971c48821631192c22f428f38efe67
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-21 00:43:22 +0300

    frontend project init

---
+++ frontend/src/app/listings/page.tsx
@@ -0,0 +1,7 @@
+export default function ListingPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>Listing Page</div>
+        </main>
+    )
+}
